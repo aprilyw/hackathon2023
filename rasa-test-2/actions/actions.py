5779018fac5c2c2dec57c975ef017289105f1d8e
April <aprilyanli38@gmail.com>
2023-08-22 19:09:44 -0500

    working document chat

--- rasa-test-2/actions/actions.py
+++ rasa-test-2/actions/actions.py
@@ -54,7 +54,11 @@ logger = getLogger(__name__)
 class ActionGPTFallback(Action):
     def __init__(self):
     #     self.llm = LLM()
-        self.agent = None
+        self.agent = GapGPT(
+                    model_type='openai',
+                    doc_type='pdf',
+                    doc_path='/Users/apwang/dev/hackathon2023/rasa-test-2/docs/OLContent.pdf',
+                )
 
     def name(self) -> str:
         return "action_gpt_fallback"
@@ -76,31 +80,9 @@ class ActionGPTFallback(Action):
 
         query = data['text']
 
-        # url_pattern = r'<(https?://\S+)>'
-        # urls = re.findall(url_pattern, query)
-        # logger.info(f'urls: {urls}')
-
-        if 'http' in query:
-            logger.info(f'query: {query}')
-            # url_pattern = r'https?://\S+\.pdf'
-            # urls = re.findall(url_pattern, query)
-
-            if not self.agent:
-                self.agent = GapGPT(
-                    model_type='openai',
-                    doc_type='webpage',
-                    doc_path=query[1:-1],
-                )
-                logger.info("agent initialized")
-                response = "url received. What would you like to know?"
-                dispatcher.utter_message(text=response)
-                return []
-                    
-        else:
-            logger.info("Not a link")
-            response = self.agent.chat(query)
-            dispatcher.utter_message(text=response)
-            return []
+        response = self.agent.chat(query)
+        dispatcher.utter_message(text=response)
+        return []
 
 
 # r’https?://\S+\.pdf’
